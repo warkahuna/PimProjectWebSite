aa3d735a4f9afbab74a0ec044e82174ecb6abfd1
unknown <mejda.rebai@esprit.tn>
2020-05-19 02:37:10 +0200

    commit 6 mejda

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MejdaService } from '../../Services/mejda.service'
+import { Product } from '../../Models/product'
 
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
-  styleUrls: ['./wishlist.component.css']
+  styleUrls: ['./wishlist.component.scss']
 })
 export class WishlistComponent implements OnInit {
-
-  constructor() { }
+  public quantity:number = 1;
+  constructor(public appService:MejdaService, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.appService.Data.cartList.forEach(cartProduct=>{
+      this.appService.Data.wishList.forEach(product=>{
+        if(cartProduct.name == product.name){
+          product.cartCount = cartProduct.cartCount;
+        }
+      });
+    });
+  }
+
+  public remove(product:Product) {
+    const index: number = this.appService.Data.wishList.indexOf(product);
+    if (index !== -1) {
+        this.appService.Data.wishList.splice(index, 1);
+    }
+  }
+
+  public clear(){
+    this.appService.Data.wishList.length = 0;
+  }
+
+  public getQuantity(val){
+    this.quantity = val.soldQuantity;
+  }
+
+  public addToCart(product:Product){
+    let currentProduct = this.appService.Data.cartList.filter(item=>item.name == product.name)[this.appService.Data.index];
+    if(currentProduct){
+      if((currentProduct.cartCount + this.quantity) <= product.availibilityCount){
+        product.cartCount = currentProduct.cartCount + this.quantity;
+      }
+      else{
+        this.snackBar.open('You can not add more items than available. In stock ' + product.availibilityCount + ' items and you already added ' + currentProduct.cartCount + ' item to your cart', '×', { panelClass: 'error', verticalPosition: 'top', duration: 5000 });
+        return false;
+      }
+    }
+    else{
+      product.cartCount = this.quantity;
+    }
+    this.appService.addToCart(product);
   }
 
 }
--- src/app/components/wishlist/wishlist.module.ts
+++ src/app/components/wishlist/wishlist.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WishlistComponent } from './wishlist.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
+
+export const routes = [
+  { path: '', component: WishlistComponent, pathMatch: 'full' }
+];
 
 
 @NgModule({
   declarations: [WishlistComponent],
   imports: [
-    CommonModule
+    RouterModule,
+    CommonModule,
+    SharedModule
   ]
 })
 export class WishlistModule { }
